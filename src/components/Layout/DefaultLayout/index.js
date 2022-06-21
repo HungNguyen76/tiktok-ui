6fa7b467667ff290af9e4ff042c7effd3e9396ba
MinhHung <nmhung.7676@gmail.com>
2022-06-21 09:58:42 +0700

    Dựng khung DefaultLayout cho dự án

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,17 @@
-import Header from "./Header";
+import Header from "~/components/Layout/components/Header";
 import Sidebar from "./Sidebar";
+import styles from './DefaultLayout.module.scss'
+import classNames from "classnames/bind";
 
+const cx = classNames.bind(styles)
 
 function DefaultLayout({children}) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header /> 
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,7 +1,6 @@
-import Header from "./Header";
+import Header from "~/components/Layout/components/Header";
 
-
-function DefaultLayout({children}) {
+function HeaderOnly({children}) {
     return (
         <div>
             <Header /> 
@@ -13,4 +12,4 @@ function DefaultLayout({children}) {
         </div>
     )
 }
-export default DefaultLayout
+export default HeaderOnly;
