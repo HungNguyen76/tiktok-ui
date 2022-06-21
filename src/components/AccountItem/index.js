ad09dfd4db640da76eba02364f9c5fcd7a7be021
MinhHung <nmhung.7676@gmail.com>
2022-06-21 16:02:11 +0700

    Xây dựng UI Header #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/e96658b61b63a72c86044df3729922d4~c5_300x300.webp?x-expires=1655960400&x-signature=JWHCfdQvK9CWNneVsrQFjgUIzjY%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
