4dc33a9ddd8153c4ce9b7da30224566272c6a0ea
MinhHung <nmhung.7676@gmail.com>
2022-06-29 15:37:53 +0700

    Build Logic of Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '../Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/e96658b61b63a72c86044df3729922d4~c5_300x300.webp?x-expires=1655960400&x-signature=JWHCfdQvK9CWNneVsrQFjgUIzjY%3D"
                 alt="Hoaa"
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,30 +1,26 @@
-import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleQuestion,
-    faCircleXmark,
     faCoins,
     faEarthAsia,
     faEllipsisVertical,
     faGear,
     faKeyboard,
     faSignOut,
-    faSpinner,
     faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
-import HeadlessTippy from '@tippyjs/react/headless';
 
 import Button from '~/components/Button';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
 import images from '~/assets/images';
-import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
 import 'tippy.js/dist/tippy.css';
-import { InboxIcon, MessageIcon, SearchIcon, UploadIcon } from '~/components/Icons';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
+import Search from '../Search'
+
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
@@ -59,13 +55,8 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([]);
     const currentUser = true;
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([]);
-        }, 0);
-    }, []);
+   
     //Handle Logic
     const handleMenuChange = (menuItem) => {
         switch (menuItem.type) {
@@ -75,7 +66,6 @@ function Header() {
             default:
         }
     };
-
 
     const userMenu = [
         {
@@ -98,46 +88,21 @@ function Header() {
             icon: <FontAwesomeIcon icon={faSignOut} />,
             title: 'Log out',
             to: '/logout',
-            separate: true
-        }
-    ]
+            separate: true,
+        },
+    ];
 
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
-                <HeadlessTippy
-                    interactive
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </PopperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search accounts and videos" spellCheck={false} />
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-                        <button className={cx('search-btn')}>
-                            <SearchIcon />
-                        </button>
-                    </div>
-                </HeadlessTippy>
+                <Search />
 
                 <div className={cx('actions')}>
                     {currentUser ? (
                         <>
-                            <Tippy delay={[0, 50]} content="Upload video" placement='bottom'>
+                            <Tippy delay={[0, 50]} content="Upload video" placement="bottom">
                                 <button className={cx('action-btn')}>
                                     <UploadIcon />
                                 </button>
@@ -153,7 +118,6 @@ function Header() {
                                     <span className={cx('badge')}>12</span>
                                 </button>
                             </Tippy>
-                            
                         </>
                     ) : (
                         <>
@@ -161,12 +125,12 @@ function Header() {
                             <Button primary>Log in</Button>
                         </>
                     )}
-                    <Menu items={currentUser ? userMenu: MENU_ITEMS} onChange={handleMenuChange}>
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <Image 
-                                src="https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/278029946_7848453055168363_9220427641853804635_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lmnqTMw6EjcAX9j6pvW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AT_AuvcjgIivQ3Q6R7xJ20q42FHYNwcRXtVVjtIlm-duJA&oe=62C08BA4" 
-                                className={cx('user-avatar')} 
-                                alt="Nguyen Minh Hung" 
+                            <Image
+                                src="https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/278029946_7848453055168363_9220427641853804635_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lmnqTMw6EjcAX9j6pvW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AT_AuvcjgIivQ3Q6R7xJ20q42FHYNwcRXtVVjtIlm-duJA&oe=62C08BA4"
+                                className={cx('user-avatar')}
+                                alt="Nguyen Minh Hung"
                             />
                         ) : (
                             <button className={cx('more-btn')}>
