42685c1be5f1b8185bf86278614a0608cf521b02
Hung Nguyen <nmhung.7676@gmail.com>
2022-06-30 09:50:03 +0700

    Refactoring and optimizing code

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -12,7 +12,7 @@ import {
     faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 import Tippy from '@tippyjs/react';
 import Button from '~/components/Button';
 import images from '~/assets/images';
@@ -95,7 +95,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
